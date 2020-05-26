611af6aae36cfef55c1f7e8733841c5522b34df8
github username <therealgracechoi@gmail.com>
2020-05-26 00:18:45 -0700

    using mapbox-gl instead

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import 'mapbox-gl/dist/mapbox-gl.css' // Import of Mapbox CSS
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+mapboxgl.accessToken = 'pk.eyJ1IjoicGluZWFwcGxlc29ucGl6emEiLCJhIjoiY2thOTJlZ2o3MGNsYjJ5cWY1cDNyb3BoYSJ9.Jbz9xMhMNnukmnmuYsozxQ';
 
 ReactDOM.render(
   <React.StrictMode>
